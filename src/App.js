30827006c2832869f46bd145d31a7154d9ca0350
Marki10 <markdarabos@gmail.com>
2018-04-23 13:16:53 +0200

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './app.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <main className="app"></main>
+    );
+  }
+}
+
+export default App;
